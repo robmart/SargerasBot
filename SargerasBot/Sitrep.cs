11b65e311055664b6f3d7589ce0eb37e03accbc7
Robmart <robmart@users.noreply.github.com>
2024-10-02 17:55:50 +0200

    Repeated sitrep

--- SargerasBot/Sitrep.cs
+++ SargerasBot/Sitrep.cs
@@ -1,5 +1,7 @@
 ﻿using Discord.WebSocket;
 using SargerasBot.Extensions;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace SargerasBot.Commands;
 
@@ -9,17 +11,27 @@ public static class Sitrep {
     public static DateOnly StartDate { get; private set; }
     public static DateOnly EndDate { get; private set; }
     public static ISocketMessageChannel Channel { get; private set; }
+    public static Timer Timer { get; private set; }
 
     public static async void Start(ISocketMessageChannel channel) {
+        if (IsActive) return;
         IsActive = true;
         Channel = channel;
 
         if (StartDate.Year < 10 || EndDate.Year < 10) {
             await StartPeriod();
         }
+        
+        var endDateTime = EndDate.ToDateTime(TimeOnly.MinValue);
+        // var endDateTime = DateTime.Now.AddMinutes(2);
+        Timer = new Timer(Convert.ToInt32(endDateTime.Subtract(DateTime.Now).TotalMilliseconds));
+        Timer.Elapsed += TimerElapsed;
+        Timer.AutoReset = false;
+        Timer.Start();
     }
 
     internal static async void Start(ISocketMessageChannel channel, DateOnly startDate, DateOnly endDate) {
+        if (IsActive) return;
         StartDate = startDate;
         EndDate = endDate;
         
@@ -31,11 +43,13 @@ public static class Sitrep {
     }
 
     public static void Stop() {
+        if (!IsActive) return;
         IsActive = false;
         Channel = null;
 
         StartDate = DateOnly.MinValue;
         EndDate = DateOnly.MinValue;
+        Timer.Stop();
     }
 
     private static async Task StartPeriod() {
@@ -43,5 +57,16 @@ public static class Sitrep {
         EndDate = DateOnly.FromDateTime(DateTime.Now.AddDays(7));
 
         await Channel.SendMessageAsync($"New sitrep period `{StartDate}` - `{EndDate}`");
+    }
+    
+    private static async void TimerElapsed(object? sender, ElapsedEventArgs e) {
+        await StartPeriod();
+        
+        var endDateTime = EndDate.ToDateTime(TimeOnly.MinValue);
+        // var endDateTime = DateTime.Now.AddMinutes(2);
+        Timer = new Timer(Convert.ToInt32(endDateTime.Subtract(DateTime.Now).TotalMilliseconds));
+        Timer.Elapsed += TimerElapsed;
+        Timer.AutoReset = false;
+        Timer.Start();
     }
 }
